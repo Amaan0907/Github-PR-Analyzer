fcc606cf26fa787752e46f9d02bd9edf09c4d35b
Amaan0907 <mohdamaan00907@gmail.com>
2026-09-14 05:16:01 +0530

    fix: update PostgreSQL port mapping and correct SQL syntax in repository migration

--- migrations/000002_create_repo.up.sql
+++ migrations/000002_create_repo.up.sql
@@ -6,12 +6,12 @@ CREATE TABLE repositories(
     name TEXT NOT NULL,
     is_active BOOLEAN NOT NULL DEFAULT true,
     created_at TIMESTAMPTZ NOT NULL DEFAULT now(),
-    updated_at TIMESTAMPTZ NOT NULL DEFAULT now()
+    updated_at TIMESTAMPTZ NOT NULL DEFAULT now(),
 
 
     CONSTRAINT fk_repositories_installation
         FOREIGN KEY (installation_id)
-        REFERENCES installations(id)
+        REFERENCES installations(id),
 
     CONSTRAINT uq_repositories_owner_name
         UNIQUE (owner,name)
